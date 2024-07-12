cd124eb6e88b66cbe134dfa4ae968c447d2cb666
taha-cmyk <151395216+taha-cmyk@users.noreply.github.com>
2024-07-12 22:45:51 +0100

    Update config.ts

--- firebase/config.ts
+++ firebase/config.ts
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAhP_BUvQLd5tlZnxkdGsCcsy7Ga5FTcxc",
-  authDomain: "techpulse-bd28e.firebaseapp.com",
-  projectId: "techpulse-bd28e",
-  storageBucket: "techpulse-bd28e.appspot.com",
-  messagingSenderId: "133907620525",
-  appId: "1:133907620525:web:66038cd29c6bb154dddc24",
-  measurementId: "G-B5HWE25N4M"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
